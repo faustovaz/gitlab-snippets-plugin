e86df8a42ab4fd27f7e3dad737a55a1e2dbac176
Jefferson Fausto Vaz <jefferson.vaz@unila.edu.br>
2018-07-12 10:29:43 -0300

    Removing comments

--- plugin.gitlab.snippets.core/src/main/java/me/faustovaz/plugin/gitlab/snippets/GitlabPlugin.java
+++ plugin.gitlab.snippets.core/src/main/java/me/faustovaz/plugin/gitlab/snippets/GitlabPlugin.java
@@ -9,64 +9,31 @@ import org.osgi.framework.BundleContext;
 
 import me.faustovaz.plugin.gitlab.view.preferences.PreferenceConstants;
 
-/**
- * The activator class controls the plug-in life cycle
- */
 public class GitlabPlugin extends AbstractUIPlugin {
 
-    // The plug-in ID
     public static final String PLUGIN_ID = "me.faustovaz.plugin.gitlab.snippets"; //$NON-NLS-1$
 
-    // The shared instance
     private static GitlabPlugin plugin;
 
     private static GitLabApi api;
 
-    /**
-     * The constructor
-     */
     public GitlabPlugin() {
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.
-     * BundleContext)
-     */
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see
-     * org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-     */
     public void stop(BundleContext context) throws Exception {
         plugin = null;
         super.stop(context);
     }
 
-    /**
-     * Returns the shared instance
-     *
-     * @return the shared instance
-     */
     public static GitlabPlugin getDefault() {
         return plugin;
     }
 
-    /**
-     * Returns an image descriptor for the image file at the given plug-in relative
-     * path
-     *
-     * @param path
-     *            the path
-     * @return the image descriptor
-     */
     public static ImageDescriptor getImageDescriptor(String path) {
         return imageDescriptorFromPlugin(PLUGIN_ID, path);
     }
